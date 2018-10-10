66b50ede4aef933c666bf6f94a5ef3ded1bf4030
Raythan <33856352+Raythan@users.noreply.github.com>
2018-10-10 11:08:15 -0300

    Update simpleStack.cpp

--- simpleStack.cpp
+++ simpleStack.cpp
@@ -2,61 +2,87 @@
 #include <stdlib.h>	// necessário p/ as funções rand() e srand()
 #include <time.h>	//necessário p/ função time()
 
-/* int numeroAleatorio(int menor, int maior) {
+int numeroAleatorio(int menor, int maior) { // Função que adiciona números aleatoriamente
     return rand()%(maior-menor+1) + menor;
-} */
+} 
 
-typedef struct stackList{
+typedef struct stackList{ // Struct para ser usada como lista...
 	int data;
 	struct stackList *next;	
 };
 
 
 
-void push(struct stackList **top, int num){
+void push(struct stackList **top, int num){ // Função que insere um elemento na cabeça da lista
+	system("cls");
 	stackList *ptr;
-	
 	ptr = (stackList *) malloc(sizeof(stackList));
 	ptr->data = num;
 	if(*top==NULL){
 		*top = ptr;
 		ptr->next = NULL;
-		printf("Entrei no if do PUSH, PTR == NULL\n\n");
 	}else{
 		ptr->next = *top;
 		*top = ptr;
-		printf("Entrei no else do PUSH, PTR != NULL\n\n");
 	}
+	printf("Numero %d adicionado com sucesso!\n\n\n", num);
 };
 
-void pop(struct stackList **top){ // CONTINUAR DAQUIIIIIIi
+void pop(struct stackList **top){ // Função que remove o último elemento inserido - cabeça do nó
+	system("cls");
 	stackList *ptr;
 	if(*top==NULL){
-		system("cls");
-		printf("Lista vazia, favor adcionar novos elementos antes de remover.\n");
+		printf("Lista vazia! Nao ha o que remover.");
 	}else{
 		ptr = *top;
-		*top = top->next;
+		*top = ptr->next;
 		free(ptr);
+		printf("Operacao realizada com sucesso!");
+	}
+	printf("\n\n\n");
+};
+
+int peek();
+
+int display(struct stackList **top){ // Função que imprime a lista pegando como parâmetro a cabeça do nó
+	system("cls");
+	struct stackList *ptr;
+	ptr = *top;
+	if(*top==NULL){
+		printf("Lista vazia!");
+	}else{
+		printf("Lista: ");
+		while(ptr!=NULL){
+			printf("%d | ", ptr->data);
+			ptr = ptr->next;
+		}
+	}
+	printf("\n\n\n");
+};
+int pushAuto(struct stackList **top, int a){ // Função que insere elementos aleatoriamente pela cabeça da lista
+	system("cls");
+	int i = 0;
+	while(i<a){
+		push(top, numeroAleatorio(1, 100));
+		i++;
 	}
 };
-int peek();
-int display();
-int pushAuto();
-int destroy();
-
-int decision(int x){
+int destroy(struct stackList **top){ // Função destroi lista chamando a função pop() que elimina elemento por elemento da cabeça da lista
 	system("cls");
-	printf("Quer continuar ou sair?\t");
-	scanf("%d", &x);
-	system("cls");
-	return x;
+	if(*top==NULL){
+		printf("Lista vazia! Nao ha o que destruir.\n\n\n");
+	}else{
+		while(*top!=NULL){
+			pop(top);
+		}
+	}
+	
 };
 
-int header(int x){
+int header(int x){ // Cabeçalho que mostra ao usuário o que ele pode fazer
 	printf("Digite a operacao desejada.\n");
 	printf("1 - Adicionar numeros manualmente.\n");
-	printf("2 - Remover numeros manualmente.\n");
+	printf("2 - Remover ultimo numero.\n");
 	printf("3 - Mostrar primeiro elemento da lista.\n");
 	printf("4 - Mostrar lista.\n");
 	printf("5 - Adicionar N numeros automaticamente.\n");
@@ -69,32 +95,36 @@ int header(int x){
 
 int main(){
 	// Variáveis
-	int tamanho = 1, num;
-	stackList *top = NULL;
+	int tamanho = 1, // condicao necessária para execução do laço até o usuário resolver sair
+	num; // Variável auxiliar para numeros manuais e quantidade de numeros inseridos automaticamente
+	stackList *top = NULL; // Ponteiro que informa a cabeça da lista
 	
-	// Aplicação
+	// Aplicaçãos
+	srand(time(NULL)); // Funcao necessária para os numeros serem inseridos sem repetição
 	while(tamanho!=0){
 		tamanho = header(tamanho);
 		
 		switch(tamanho){
 			case 1:
-				printf("Digite o numero para adicionar: ");
+				printf("Digite o numero para adicionar: "); // Pergunta para saber qual numero adicionar
 				scanf("%d", &num);
-				push(&top, num);
-				//tamanho = decision(tamanho);
-				tamanho = 1;
+				push(&top, num); // Função insere números manualmente
 				break;
 			case 2:
-				pop(&top);
-				tamanho = decision(tamanho);
+				pop(&top); // Função que remove o último elemento da lista
 				break;
 			case 3:
 				break;
 			case 4:
+				display(&top); // Função que imprime a lista
 				break;
 			case 5:
+				printf("Quantos numeros quer adicionar? "); // Pergunta para saber quantidade a ser inserida
+				scanf("%d", &num);
+				pushAuto(&top, num); // Função explicada anteriormente
 				break;
 			case 6:
+				destroy(&top); // Função destroi lista
 				break;	
 		}
 	}
