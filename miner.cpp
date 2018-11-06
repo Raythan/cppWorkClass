6f82e06728ac21b6335d59125e0592494ed97dff
Raythan <33856352+Raythan@users.noreply.github.com>
2018-11-06 19:58:48 -0200

    Update miner.cpp

--- miner.cpp
+++ miner.cpp
@@ -1,1 +1,20 @@
 //On going
+#include <stdbli.h>
+#include <stdio.h>
+#include <time.h>
+#define MAX 10
+int nRange(int menor, int maior) {
+    return rand()%(maior-menor+1) + menor;
+}
+void printShot();
+void printTarget();
+int preenche();
+
+
+int main(){
+	int vt[MAX][MAX];	
+	
+	
+	
+	return 0;
+}
