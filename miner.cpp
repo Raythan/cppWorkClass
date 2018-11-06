eb8c4fc57def2808932c2c6a6290f42c4b0641ad
Raythan <33856352+Raythan@users.noreply.github.com>
2018-11-06 20:48:10 -0200

    Update miner.cpp

--- miner.cpp
+++ miner.cpp
@@ -1,4 +1,3 @@
-//On going
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
@@ -6,15 +5,52 @@
 int nRange(int menor, int maior) {
     return rand()%(maior-menor+1) + menor;
 }
-void printShot();
-void printTarget();
-int preenche();
+void printShotable(int vt[][MAX]){
+	int i, j;
+	for(i=0;i<MAX;i++){
+		printf("| ");
+		for(j=0;j<MAX;j++){
+			printf("%d | ", vt[i][j]);
+		}
+		printf("\n");
+	}
+};
+void printTarget(int vt[][MAX]){
+	int i, j;
+	printf("\n\n  | ");
+	for(j=0;j<MAX;j++){
+		printf("%d | ", j);
+	}
+	printf("\n");
+	for(i=0;i<MAX;i++){
+		printf("%d | ", i);
+		for(j=0;j<MAX;j++){
+			if(vt[i][j]!=0){
+				printf("S | ");
+			}else{
+				printf("N | ");
+			}
+			
+		}
+		printf("\n");
+	}
+};
+int preenche(int vt[][MAX]){
+	int i, j;
+	for(i=0;i<MAX;i++){
+		for(j=0;j<MAX;j++){
+			vt[i][j] = nRange(0, 1);
+		}
+	}
+};
 
 
 int main(){
-	int vt[MAX][MAX];	
-	
-	
-	
+	//Variáveis
+	int vt[MAX][MAX], vt2[MAX][MAX];	
+	srand(time(NULL));
+	preenche(vt);
+	printShotable(vt);
+	printTarget(vt);
 	return 0;
 }
