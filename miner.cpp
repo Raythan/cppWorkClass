6f98a2e67bbd16be68be0731c0f7a032de20d4bb
Raythan <33856352+Raythan@users.noreply.github.com>
2018-11-07 21:18:03 -0200

    Update miner.cpp

--- miner.cpp
+++ miner.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <time.h>
 #define MAX 10
+
 int nRange(int menor, int maior) {
     return rand()%(maior-menor+1) + menor;
 }
@@ -38,11 +39,12 @@ void printTarget(int vtv[][MAX]){
 		printf("\n");
 	}
 };
-void preenche(int vt[][MAX]){
+void preenche(int vt[][MAX], int vt2[][MAX]){
 	int i, j;
 	for(i=0;i<MAX;i++){
 		for(j=0;j<MAX;j++){
 			vt[i][j] = nRange(0, 1);
+			vt2[i][j] = vt[i][j];
 		}
 	}
 };
@@ -55,21 +57,30 @@ void preencheAlvo(int vt[][MAX], int vt2[][MAX], int x, int y, int i){
 		vt2[x][y] = 4;
 	}
 };
-int check(int vt[][MAX], int x, int y){
-	if(vt[x][y]==1){
-		system("cls");
-		printf("Perdeu, Acertou a mina!\n");
-		return 0;
-	} 
-	return 1;
+int check(int vt[][MAX], int vt2[][MAX], int x, int y){
+	int i, j;
+	for(i=0;i<MAX;i++){
+		for(j=0;j<MAX;j++){
+			if(vt[x][y]==1){
+				system("cls");
+				printf("Perdeu, Acertou a mina!\n");
+				return 0;
+			} 
+			if(vt2[i][j]==0){
+				return 1;	
+			}
+		}
+	}
+	printf("Parabens, vencedor!!\n\n");
+	return 0;
 }
 
 int main(){
 	//Variáveis
-	int vt[MAX][MAX], vt2[MAX][MAX], x, y, i=1;	
+	int vt[MAX][MAX], vt2[MAX][MAX], x, y, i=1;
 	srand(time(NULL));
-	preenche(vt);
-	printShotable(vt);
+	preenche(vt, vt2);
+	//printShotable(vt);
 	printTarget(vt2);
 	while(i!=0){
 		printf("Linha: ");
@@ -79,7 +90,7 @@ int main(){
 		system("cls");
 		preencheAlvo(vt, vt2, x, y, i);
 		printf("\nUltima jogada: %d %d\n%d\n\n", x, y, i);
-		i = check(vt, x ,y);
+		i = check(vt, vt2, x ,y);
 		if(i==0){
 			printShotable(vt);
 			printTarget(vt2);	
