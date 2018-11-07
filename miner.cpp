2bd6d8c32153a5cdfbd2694e254c355066d9cedf
Raythan <33856352+Raythan@users.noreply.github.com>
2018-11-06 22:11:06 -0200

    Update miner.cpp

--- miner.cpp
+++ miner.cpp
@@ -47,25 +47,31 @@ void preenche(int vt[][MAX]){
 	}
 };
 void preencheAlvo(int vt[][MAX], int vt2[][MAX], int x, int y, int i){
-	if(vt2[x][y]==3){
+	if((vt2[x][y]==3&&vt[x][y]==0)){
 		printf("Voce ja jogou nessa posicao.");
 	}else if(vt[x][y]==0){
 		vt2[x][y] = 3;	
 	}else if(vt[x][y]==1){
-		printf("Perdeu, acertou a mina.");
 		vt2[x][y] = 4;
-		i = 0;
 	}
 };
+int check(int vt[][MAX], int x, int y){
+	if(vt[x][y]==1){
+		system("cls");
+		printf("Perdeu, Acertou a mina!\n");
+		return 0;
+	} 
+	return 1;
+}
 
 int main(){
 	//Variáveis
 	int vt[MAX][MAX], vt2[MAX][MAX], x, y, i=1;	
 	srand(time(NULL));
 	preenche(vt);
+	printShotable(vt);
+	printTarget(vt2);
 	while(i!=0){
-		printShotable(vt);
-		printTarget(vt2);
 		printf("Linha: ");
 		scanf("%d", &x);
 		printf("Coluna: ");
@@ -73,6 +79,13 @@ int main(){
 		system("cls");
 		preencheAlvo(vt, vt2, x, y, i);
 		printf("\nUltima jogada: %d %d\n%d\n\n", x, y, i);
+		i = check(vt, x ,y);
+		if(i==0){
+			printShotable(vt);
+			printTarget(vt2);	
+		}else{
+			printTarget(vt2);
+		}
 	}
 	return 0;
 }
